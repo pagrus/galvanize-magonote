6efc4202b29030b2a5c79ceaf8c4b3e4d6cb3adc
pagrus <galvanize@nebulos.us>
2018-04-30 16:49:14 +0000

    start your day with some good news

--- magostart.sh
+++ magostart.sh
@@ -5,4 +5,10 @@ ssh -NfL 65432:localhost:65432 magonote
 
 # sshfs mount ec2 vol
 sshfs magonote:galvanize-magonote ~/Desktop/ec2-magonote/
- 
+
+# status of downloads
+echo "files in html/post_pages:"
+ssh magonote "ls -1 galvanize-magonote/html/post_pages | wc -l"
+echo "files under 2000 bytes:"
+ssh magonote "find galvanize-magonote/html/post_pages -size -2000c"
+
