c1c187843590ae1ab8079f4fa5b4455219acf6ae
pagrus <galvanize@nebulos.us>
2018-05-05 02:02:07 +0000

    back where we started, urgggg

--- prod/magonote_get_user_info_from_post_html_db.py
+++ prod/magonote_get_user_info_from_post_html_db.py
@@ -24,22 +24,12 @@ cur.execute("SELECT post_html FROM itch_post_html LIMIT 500")
 for row in cur:
     html = row[0]
     soup = bs(html, "lxml")
-    cp_divs = soup.find_all("div", class_='community_post')
-    ph_divs = soup.find_all("div", class_='post_header')
-    if len(cp_divs) > 0:
-        data_post = cp_divs[0]['data-post']  
-        if len(data_post) > 0:
-            user_id_split = data_post.split(',')
-            user_id = user_id_split[0][11:]
-            
-    if len(ph_divs) > 0:
-        ph_span = ph_divs[0].find_all('span')
-        if len(ph_span[0]) > 0:
-            user_name = ph_span[0].string
-            try: 
-                user_url = ph_span[0]('a')[0]['href'][9:]
-            except:
-                user_url = ''
+    pa_span = soup.find_all("span", class_='post_author')
+    if len(pa_span) > 0:
+        print(pa_span[0])
+    cp_div = soup.find_all("div", class_='community_post')
+    if len(cp_div) > 0:
+        print(cp_div[0])
 
 cur.close()
 db_connection.close()
