99c4dc9764253805021743b09c215435c8a2f220
pagrus <ororo@nebulos.us>
2019-03-29 08:50:35 -0700

    rethinking things, add scraper

---
+++ src/scrape.py
@@ -0,0 +1,27 @@
+import requests
+
+'''
+
+Get HTML, stash in appropriate dirs
+Show progress, other stats
+
+'''
+
+def get_game_pages(max_pages):
+    '''
+    gets game description pages, stopping after max_pages
+    '''
+    pass
+
+def get_tag_list:
+    '''
+    get a list of tags and stats
+    '''
+    pass
+
+def get_comment_pages(start, end, bogo):
+    '''
+    get comment page HTML, in a range specified by start and end
+    fetch randomly by default
+    '''
+    pass
