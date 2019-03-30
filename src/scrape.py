e2cd717c10a60a9737fc7740188919a3c3c591c3
pagrus <ororo@nebulos.us>
2019-03-30 13:00:20 -0700

    woah there"

--- src/scrape.py
+++ src/scrape.py
@@ -19,9 +19,11 @@ def get_tag_list:
     '''
     pass
 
-def get_comment_pages(start, end, bogo):
+def get_comment_page(index):
     '''
-    get comment page HTML, in a range specified by start and end
-    fetch randomly by default
+    get comment page HTML
+    
     '''
-    pass
+    base_url = 'https://itch.io/post/'
+    post_url = base_url + str(index)
+    return requests.get(post_url)
